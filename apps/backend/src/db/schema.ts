767ed79a46e19235262145de03760d7a1d6c1a04
greenina <igreen0485@gmail.com>
2024-07-08 13:25:29 +0900

    Update schema

--- apps/backend/src/db/schema.ts
+++ apps/backend/src/db/schema.ts
@@ -1,34 +1,45 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const ChoiceSchema = new Schema({
-  choiceId: String,
-  choiceText: String,
-  timestamp: { type: Date, default: Date.now }
-});
+const AnswerSchema = new Schema({
+  question: {type: Schema.Types.ObjectId, ref: 'Question', required: true},
+  content: {type: String, default: Date.now},
+  createdAt: {type: Date, default: Date.now}
+})
 
 const QuestionSchema = new Schema({
-  questionId: String,
-  questionText: String,
-  userAnswer: String,
-  timestamp: { type: Date, default: Date.now }
+  _id: {type: Schema.Types.ObjectId, required: true},
+  content: {type: String, required: true},
+  plausibleAnswers: [AnswerSchema],
+  userAnswer: AnswerSchema,
+  createdAt: { type: Date, default: Date.now }
 });
+
+const ThreadSchema = new Schema({
+  theme: {type: Schema.Types.ObjectId, ref: 'Theme', required: true},
+  questions: [QuestionSchema],
+  createdAt: {type: Date, default: Date.now}
+})
 
 const ThemeSchema = new Schema({
-  themeId: String,
-  name: String,
-  questions: [QuestionSchema],
-  choices: [ChoiceSchema]
+  name: {type: String, required: true},
+  description: {type: String},
+  threads: [ThreadSchema],
+  createdAt: {type: Date, default: Date.now}
 });
 
-const SessionSchema = new Schema({
-  sessionId: String,
-  themes: [ThemeSchema]
-});
+const HistoryItemSchema =  new Schema({
+  question: {type: Schema.Types.ObjectId, ref: 'Question'},
+  answer: {type: Schema.Types.ObjectId, ref: 'Answer'}
+})
+
 
 const UserSchema = new Schema({
   name: String,
-  background: String,
-  initialNarrative: String,
-  sessions: [SessionSchema]
+  selfNarrative: {type: String, required: true},
+  themes: [ThemeSchema],
+  history: [HistoryItemSchema],
+  createdAt: {type: Date, default: Date.now}
 });
+
+export {AnswerSchema, QuestionSchema, ThreadSchema, ThemeSchema, HistoryItemSchema, UserSchema}
