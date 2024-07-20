89b2b7ce001a306873b14e3e0847eca625553e55
greenina <igreen0485@gmail.com>
2024-07-20 18:53:46 +0900

    Update schema

--- apps/backend/src/config/schema.ts
+++ apps/backend/src/config/schema.ts
@@ -1,74 +1,112 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const AnswerSchema = new Schema({
-  aContent: {type: String, required: true},
-  createdAt: {type: Date, default: Date.now},
-  updatedAt: {type: Date}
-})
-
-const GranularItemSchema = new Schema({
-  labelContent: {type: String, required: true},
-  uGenerated: {type: Boolean, required: true},
-  selected: {type: Boolean, default: false}
-})
-
-// TODO: 언어 (localized text)
-const QuestionSchema = new Schema({
-  user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-  qContent: {type: String, required: true},
-  granularItems: [GranularItemSchema],
-  userAnswer: AnswerSchema,
-  selected: Boolean,
-  createdAt: { type: Date, default: Date.now }
-});
-
-const ThreadSchema = new Schema({
-  user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-  theme: {type: Schema.Types.ObjectId, ref: 'Theme', required: true},
-  questions: [QuestionSchema],
-  createdAt: {type: Date, default: Date.now}
-})
-
-const ThemeSchema = new Schema({
-  user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-  repContent: {type: String, required: true},
-  altContent: {type: [String], default: []},
-  description: {type: String, default:''},
-  threads: {type: [ThreadSchema], default: []},
-  createdAt: {type: Date, default: Date.now},
-  saved: {type: Boolean, required: true},
-  activated: {type: Boolean, required: true}
-});
-
-const HistoryItemSchema =  new Schema({
-  user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-  historyType: {type: String},
-  question: {type: Schema.Types.ObjectId, ref: 'Question'},
-  content: {type: String}
-})
-
-
-const UserSchema = new Schema({
-  name: {type: String, required: true},
-  selfNarrative: {type: String, required: true},
-  background: {type: String, required: true},
-  themes: {type: [ThemeSchema], default: []},
-  history: {type: [HistoryItemSchema], default: []},
-});
-
-AnswerSchema.set('timestamps', true);
-QuestionSchema.set('timestamps', true)
-ThreadSchema.set('timestamps', true)
-ThemeSchema.set('timestamps', true)
-HistoryItemSchema.set('timestamps', true)
-GranularItemSchema.set('timestamps', true)
-
-
-const Question = mongoose.model('Question', QuestionSchema)
-const Theme = mongoose.model('Theme', ThemeSchema)
-const HistoryItem = mongoose.model('HistoryItem', HistoryItemSchema)
-const User = mongoose.model('User', UserSchema)
-
-
-export {Question, HistoryItem, User, Theme}
+const AIFeedbackSchema = new Schema({
+  // TBD
+ })
+ 
+ const TypeAScaffoldingSchema = new Schema({
+   tried: {type: Boolean, required: true},
+   unselected_keywords: {type: [String], required: function(){return this.tried;}},
+   selected_keywords: {
+     type: [{
+       content: { type: String, required: true },
+       ai_generated: { type: Boolean, required: true }
+     }],
+     required: function() { return this.tried; }
+   },
+   generated_sentence: {
+     type: [{
+       type: String, 
+       selected: Boolean
+     }],
+     required: function(){return this.tried;}
+   },
+   ai_synthesize: {type: String, required: function(){return this.tried;}},
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ })
+ 
+ const TypeBScaffoldingSchema = new Schema({
+   tried: {type: Boolean, required: true},
+   qa_set: {
+     type: [{
+       question: {type: String, required: true},
+       selected: {type: [String], required: true},
+       unselected: {type: [String], required: true},
+     }],
+     required: function() { return this.tried; }
+   },
+   ai_synthesize: {type: String, required: function(){return this.tried;}} ,
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ })
+ 
+ const TypeCScaffoldingSchema = new Schema({
+   tried: {type: Boolean, required: true},
+   prompt_set: {
+     type: [{
+       tip: {type: String, required: true},
+       prompts: {
+         type: [{type: String, selected: Boolean}],
+         required: true,
+       }
+     }],
+     required: function(){return this.tried;}
+   },
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ })
+ 
+ const ScaffoldingDataSchema = new Schema({
+   typeA: {
+     type: TypeAScaffoldingSchema,
+     required: true
+   },
+   typeB: {
+     type: TypeBScaffoldingSchema,
+     required: true
+   },
+   typeC: {
+     type: TypeCScaffoldingSchema,
+     required: true
+   },
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ })
+ 
+ const ThreadItemSchema = new Schema({
+   uid: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+   question: {type: String, required: true},
+   scaffoldingData: {
+     type: ScaffoldingDataSchema,
+     required: true
+   },
+   response: {
+     type: String,
+     required: true
+   },
+   history_information: {type: [String], required: true},
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ });
+ 
+ ThreadItemSchema.set('timestamps', true)
+ 
+ const UserSchema = new Schema({
+   name: {type: String, required: true},
+   initial_narrative: {type: String, required: true},
+   background: {type: String, required: true},
+   thread: {type: [{type: Schema.Types.ObjectId, ref: 'ThreadItem'}], default: [], required: true},
+   createdAt: {type: Date, default: Date.now},
+   updatedAt: {type: Date}
+ });
+ 
+ UserSchema.set('timestamps', true);
+ 
+ 
+ const ThreadItem = mongoose.model('ThreadItem', ThreadItemSchema)
+ const User = mongoose.model('User', UserSchema)
+ 
+ 
+ export {User, ThreadItem}
